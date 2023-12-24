7f08ea4ee02909fc6c043e66669f803f9bb78f7b
dhwanish-3 <108540722+dhwanish-3@users.noreply.github.com>
2023-12-24 08:27:53 +0530

    major progress=popup

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import { Swiper, SwiperSlide } from "swiper/react";
+import Image from "next/image";
+import ArrowUp from "../../public/arrow-up.svg";
+import ArrowDown from "../../public/arrow-down.svg";
 import "swiper/css";
 import "./Login.css";
 
@@ -18,70 +21,87 @@ export default function LoginPopUp() {
     "brown",
     "gray",
   ];
-  const avatars = ["img1", "img2", "img3"];
+  const avatars = [
+    ArrowDown,
+    ArrowDown,
+    ArrowDown,
+    ArrowDown,
+    ArrowDown,
+    ArrowDown,
+    ArrowDown,
+    ArrowDown,
+  ];
   return (
-    <div className="login popup">
-      <span>Login</span>
-      <div className="avatar-slider">
-        <Swiper
-          centeredSlides={true}
-          loop={true}
-          spaceBetween={0}
-          effect="coverflow"
-          grabCursor={true}
-          slidesPerView="auto"
-          coverflowEffect={{
-            rotate: 0,
-            stretch: 0,
-            depth: 300,
-            modifier: 1,
-            slideShadows: false,
-          }}
-          pagination={{
-            el: ".swiper-pagination",
-          }}
-        >
-          {colors.map((color) => {
-            return (
-              <SwiperSlide>
-                <SingleAvatar color={color} />
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
+    <div className="background">
+      <div className="avatar-popup">
+        <div className="login">
+          <span>Login</span>
+          <div className="avatar-picker">
+            <Image src={ArrowDown} alt="" />
+            <div className="avatar-slider">
+              <Swiper
+                centeredSlides={true}
+                loop={true}
+                spaceBetween={0}
+                effect="coverflow"
+                grabCursor={true}
+                slidesPerView="auto"
+                coverflowEffect={{
+                  rotate: 0,
+                  stretch: 0,
+                  depth: 300,
+                  modifier: 1,
+                  slideShadows: false,
+                }}
+                pagination={{
+                  el: ".swiper-pagination",
+                }}
+              >
+                {avatars.map((avatar) => {
+                  return (
+                    <SwiperSlide>
+                      <SingleAvatar image={avatar} color={colors[2]} />
+                    </SwiperSlide>
+                  );
+                })}
+              </Swiper>
+            </div>
+            <div className="avatar-slider">
+              <Swiper
+                centeredSlides={true}
+                loop={true}
+                spaceBetween={0}
+                effect="coverflow"
+                grabCursor={true}
+                slidesPerView="auto"
+                coverflowEffect={{
+                  rotate: 0,
+                  stretch: 0,
+                  depth: 300,
+                  modifier: 1,
+                  slideShadows: false,
+                }}
+                pagination={{
+                  el: ".swiper-pagination",
+                }}
+              >
+                {colors.map((color) => {
+                  return (
+                    <SwiperSlide>
+                      <SingleAvatar color={color} />
+                    </SwiperSlide>
+                  );
+                })}
+              </Swiper>
+            </div>
+            <Image src={ArrowUp} alt="" />
+          </div>
+          <div className="username">
+            <input type="text" required placeholder="Enter username" />
+          </div>
+          <button className="username-submit">Submit</button>
+        </div>
       </div>
-      <div className="avatar-slider">
-        <Swiper
-          centeredSlides={true}
-          loop={true}
-          spaceBetween={0}
-          effect="coverflow"
-          grabCursor={true}
-          slidesPerView="auto"
-          coverflowEffect={{
-            rotate: 0,
-            stretch: 0,
-            depth: 300,
-            modifier: 1,
-            slideShadows: false,
-          }}
-          pagination={{
-            el: ".swiper-pagination",
-          }}
-        >
-          {colors.map((color) => {
-            return (
-              <SwiperSlide>
-                <SingleAvatar color={color} />
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
-      </div>
-      <div className="username">
-        <input type="text" required placeholder="Enter username" />
-      </div>
-      <button className="username-submit">Submit</button>
     </div>
   );
 }
@@ -97,7 +117,8 @@ function SingleAvatar({ color, image }: any) {
   if (image) {
     return (
       <div className="single-avatar">
-        <img src="image" alt="img" />
+        <div className="color" style={{ backgroundColor: color }}></div>
+        <Image src={image} alt="img" />
       </div>
     );
   }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
+"use client";
+
 import LoginPopUp from "./login/page";
 import NotesPage from "./notes/page";
 
 export default function Home() {
+  const handleClick = () => {
+    document.querySelector(".avatar-popup ")?.classList.add("show");
+  };
   return (
     <div>
       <NotesPage />
+      <button onClick={handleClick}>Popup</button>
       <LoginPopUp />
     </div>
   );
